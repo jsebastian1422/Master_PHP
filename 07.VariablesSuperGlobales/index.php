efa94b350a97281a0213abac3d06688bb8778948
Practicante Sena Sistemas <sistemas.practicante@lan.cdo-sa.com>
2019-01-10 18:00:03 -0500

    Dia 2 del curso

---
+++ 07.VariablesSuperGlobales/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/*Variables super globales*/
+
+//Variables de servidor.
+
+//Direccion IP.
+
+echo '<h1>' . $_SERVER['SERVER_ADDR'] . '</h1>';
+
+//Nombre del servidor.
+
+echo '<h1>' . $_SERVER['SERVER_NAME'] . '</h1>';
+
+//Nombre del servidor.
+
+echo '<h1>' . $_SERVER['SERVER_SOFTWARE'] . '</h1>';
+
+//Navegador web del servidor.
+
+echo '<h1>' . $_SERVER['HTTP_USER_AGENT'] . '</h1>';
+
+//Direccion ip del cliente.
+
+echo '<h1>' . $_SERVER['REMOTE_ADDR'] . '</h1>'; 
+?>
